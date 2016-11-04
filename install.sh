ed25f60d1223e0eadc60614340d4a14a6af5ffab
root <root@beaglebone.localdomain>
2016-11-04 19:04:51 +0000

    added the get queue and add queue functionality to the server

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo installing socket.io
-#npm install socket.io
+#npm -g install socket.io
 echo installing ace script editor package
-git clone https://github.com/ajaxorg/ace-builds/
+#git clone https://github.com/ajaxorg/ace-builds/
+echo installing express
+#npm -g install express
--- web.js
+++ web.js
@@ -2,13 +2,31 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var queue = [];
+var queueNames = ['hello','world','i','hope','this','works'];
+var currentName = 'none';
+var currentlyRunning;
+var timeBetweenPatterns = 240; // in seconds
+
 
 app.use('/', express.static(__dirname + '/UI'));
 
 io.on('connection', function(socket) {
     // Code here will be run when a user connects.
     console.log("[SOCKET] Connection started.");
-
+    socket.on('getQueue', function(){
+        console.log("client requested the queue");
+        current = currentName;
+        queue = queueNames;
+        time = 53;
+        timeBetweenPatterns = timeBetweenPatterns;
+    });
+    
+    socket.on('addQueue', function(x){
+       console.log("add to the queue"+x);
+       queueNames.push(x.name);
+       queue.push(x.data);
+    });
 
 
 
